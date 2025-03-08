486c4221164ed193caf10cf26f15f103ec300399
celtic93 <egorka80@list.ru>
2025-03-08 17:57:25 +0300

    Added server with config #1

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/celtic93/chat-server/internal/config"
+	server "github.com/celtic93/chat-server/internal/grpc-server"
+	desc "github.com/celtic93/chat-server/pkg/v1/chat"
 )
 
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	cfg := config.MustLoad()
+
+	conn, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Host, cfg.Port))
+	if err != nil {
+		log.Fatal(color.RedString("failed to serve grpc server: %v", err))
+	}
+
+	log.Print(color.GreenString("UserAPI grpc server listening on: %s", conn.Addr().String()))
+
+	gsrv := grpc.NewServer()
+	reflection.Register(gsrv)
+
+	desc.RegisterChatV1Server(gsrv, &server.Server{})
+
+	if err = gsrv.Serve(conn); err != nil {
+		log.Fatal(color.RedString("failed to serve grpc server: %v", err))
+	}
 }
---
+++ internal/grpc-server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"context"
+	"log"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/celtic93/chat-server/pkg/v1/chat"
+)
+
+type Server struct {
+	desc.UnimplementedChatV1Server
+}
+
+// Create: creates chat
+func (s *Server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("server.Create Chat id: %s", req.GetUsernames())
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+// SendMessage: sends message to chat
+func (s *Server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("server.Update Chat id: %s", req.GetFrom())
+
+	return &emptypb.Empty{}, nil
+}
+
+// Delete: deletes chat
+func (s *Server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("server.Delete Chat id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
