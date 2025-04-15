b8d69bbf72ad57203e3cfb483da65070e0cf1603
celtic93 <egorka80@list.ru>
2025-04-15 09:46:36 +0300

    Updated migrations #3

--- migrations/20250313113530_create_chats_table.sql
+++ migrations/20250313113530_create_chats_table.sql
@@ -3,16 +3,16 @@
 CREATE TABLE chats
 (
     id bigserial primary key,
-    created_at timestamp(0) default CURRENT_TIMESTAMP,
-    updated_at timestamp(0)
+    created_at timestamp(0) not null default CURRENT_TIMESTAMP,
+    updated_at timestamp(0) not null
 );
 CREATE TABLE chats_users
 (
     id bigserial primary key,
     chat_id int references chats(id) on delete cascade,
-    username varchar(64) not null,
-    created_at timestamp(0) default CURRENT_TIMESTAMP,
-    updated_at timestamp(0)
+    user_id int not null,
+    created_at timestamp(0) not null default CURRENT_TIMESTAMP,
+    updated_at timestamp(0) not null
 );
 -- +goose StatementEnd
 
--- migrations/20250313113537_create_messages_table.sql
+++ migrations/20250313113537_create_messages_table.sql
@@ -4,10 +4,10 @@ CREATE TABLE messages
 (
     id bigserial primary key,
     chat_id int references chats(id) on delete cascade,
-    username varchar(64) not null,
+    user_id int not null,
     text text not null,
-    created_at timestamp(0) default CURRENT_TIMESTAMP,
-    updated_at timestamp(0)
+    created_at timestamp(0) not null default CURRENT_TIMESTAMP,
+    updated_at timestamp(0) not null
 );
 -- +goose StatementEnd
 
